a45bd7c886214d703276e4b3083855d3e386b604
Jaden Sierra <JadenSierra31@gmail.com>
2021-12-21 15:20:10 -0800

    inital commit

---
+++ Making_Connections/script.js
@@ -0,0 +1,33 @@
+let toddRequest = document.querySelector("#todd")
+let philRequest = document.querySelector("#phil")
+let requests = [toddRequest,philRequest]
+
+let newRequestsAmount = document.querySelector("#requests");
+let currentRequestsAmount = 2;
+
+let currentConnectionsAmount = 500;
+let newConnectionsAmount = document.querySelector("#connections")
+
+let createInput = document.createElement("input")
+
+
+let userName = document.querySelector("#userName")
+
+function deny(index) {
+    currentRequestsAmount--;
+    newRequestsAmount.innerText = currentRequestsAmount;
+    requests[index].remove();
+}
+
+function accept(index) {
+    currentRequestsAmount--;
+    newRequestsAmount.innerText = currentRequestsAmount;
+    currentConnectionsAmount++;
+    newConnectionsAmount.innerText = currentConnectionsAmount;
+    requests[index].remove();
+}
+
+function changeName() {
+
+    userName.innerText = "Mary Jane";
+}
